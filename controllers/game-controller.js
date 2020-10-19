c9ed91e8fefa9de61375af69a3040ac8152bb512
BudiartoSalim <budiarto.salim91@gmail.com>
2020-10-19 14:41:25 +0700

    shop rework to make use of the new shop schema, and refactored several parts into async await

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -1,57 +1,80 @@
 const { User, ItemShop, Monster, Question, UserStatus } = require('../models')
 class GameController {
 
-  //put /shop/:userid/:item
+  //put /shop/:itemid
   static async upgradeStat(req, res, next) {
-    const item = await ItemShop.findOne({
-      where: {
-        id: req.params.item
-      }
-    })
-    const userStatus = await UserStatus.findOne({
-      where: {
-        UserId: req.payload.id
-      }
-    })
-    const userStatUpdate = await UserStatus.update({
-      atk: (userStatus.atk + parseInt(req.body.stat)),
-      hp: (userStatus.hp + parseInt(req.body.stat)),
-      def: (userStatus.def + parseInt(req.body.stat)),
-      money: (userStatus.money - parseInt(item.price))
-    }, {
-      where: {
-        id: userStatus.id
-      }
-    })
-      .then(data => {
-        res.status(200).json({
-          message: 'Successfully upgrade your status',
-          userStatus
-        })
+    const difficultyCap = 2; //current difficulty cap
+    try {
+      let item = await ItemShop.findOne({
+        where: {
+          id: req.params.itemid
+        }
       })
-      .catch(err => {
-        next(err)
-        // res.status(500).json(err)
+      const userStatus = await UserStatus.findOne({
+        where: {
+          UserId: req.payload.id
+        }
       })
+
+      //safeguard to ensure user cannot buy more difficulty upgrade when it reaches the cap
+      if (userStatus.difficulty >= difficultyCap && item.difficulty >= 0) {
+        item.difficulty = 0;
+        res.status(400).json({ message: 'Already at max difficulty' })
+      } else {
+        item.price = item.price + (item.price * item.difficulty); //simple formula to make difficulty price scale
+        if (userStatus.money >= item.price) {
+          const userStatUpdate = await UserStatus.update({
+            atk: (userStatus.atk + item.atk),
+            hp: (userStatus.hp + item.hp),
+            def: (userStatus.def + item.def),
+            money: (userStatus.money - item.price),
+            maxDifficulty: userStatus.difficulty + item.difficulty
+          }, {
+            where: {
+              id: userStatus.id
+            },
+            returning: true,
+            plain: true
+          })
+          res.status(200).json({
+            message: 'Successfully upgrade your status',
+            userStatus: userStatUpdate[1]
+          })
+        } else {
+          res.status(400).json({
+            message: 'money not enough'
+          })
+        }
+      }
+    } catch (err) {
+      next(err);
+    }
   }
-  static updateUserDifficult(req, res, next) {
-    UserStatus.update({
-      currentDifficulty: req.body.difficulty,
-      maxDifficulty: req.body.difficulty
-    }, {
-      where: {
-        UserId: req.payload.id
-      }
-    })
-      .then(data => {
+
+  // PATCH /users/difficulty
+  static async updateUserDifficulty(req, res, next) {
+    try {
+      req.body.difficulty = parseInt(req.body.difficulty); //parses difficulty to integer
+      const currentStat = await UserStatus.findOne({ where: { UserId: req.payload.id } });
+
+      //safeguard to make sure the request is valid difficulty
+      if (req.body.difficulty > currentStat.maxDifficulty || req.body.difficulty < 0 || isNaN(req.body.difficulty)) {
+        res.status(400).json({ message: 'Invalid difficulty.' });
+      } else {
+        await UserStatus.update({ currentDifficulty: req.body.difficulty },
+          {
+            where: { UserId: req.payload.id },
+            returning: true,
+            plain: true
+          });
         res.status(200).json({
-          message: 'Your Difficulty Status has Been updated'
+          message: 'Your Difficulty Status has Been updated',
+          difficulty: req.body.difficulty
         })
-      })
-      .catch(err => {
-        next(err)
-        // res.status(500).json(err)
-      })
+      }
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = GameController;
